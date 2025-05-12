66711ce751d7c83cb93c0e5f08cdaef7f93166b0
murjanii <153760405+murjanii@users.noreply.github.com>
2025-05-12 13:23:57 +0530

    created a submission

--- script.js
+++ script.js
@@ -9,15 +9,30 @@ function convertToRoman(num) {
       6:['I', 1]
     };
 
-  //your code here
+
+let n=798;
+let ans="";
+while(n!=0){
+   
+   for(leto i in romanMap){
+
+       let value=romanMap[i][0];
+       let num=romanMap[i][1];
+
+
+        if(num<=n){
+
+           ans=ans+value;
+            n=n-num;
+             break;
+            
+}
+        
+
+
 
 }
-// You can test your code by running the above function and printing it to console by pressing the run button at the top. To run it with input 36, uncomment the following line
-
-// console.log(convertToRoman(36));
 
 
 
-
-// do not edit below this line
-module.exports = convertToRoman
+}
